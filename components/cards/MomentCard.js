039fe4da5a16fbdee6aad8e8e48350e2c3f0c1b8
Johnny Saniat <Jcsaniat@gmail.com>
2023-11-13 20:39:33 -0600

    Created MilestoneCard, MomentCard, and Mock Data

---
+++ components/cards/MomentCard.js
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteMoment } from '../../api/momentData';
+
+function MomentCard({ momentObj, onUpdate }) {
+  const deleteThisMoment = () => {
+    if (window.confirm(`Delete ${momentObj.name}?`)) {
+      deleteMoment(momentObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={momentObj.image} alt={momentObj.name} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{momentObj.name}</Card.Title>
+        <Card.Text>{momentObj.category}</Card.Text>
+        <Card.Text>{momentObj.week}</Card.Text>
+        <Link href={`/moment/${momentObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/moment/edit/${momentObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisMoment} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+MomentCard.propTypes = {
+  momentObj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    week: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    category: PropTypes.string,
+    milestone_id: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default MomentCard;
---
+++ pages/milestones.js
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getMilestones } from '../api/milestoneData';
+import { useAuth } from '../utils/context/authContext';
+import MilestoneCard from '../components/cards/MilestoneCard';
+
+function ShowMilestones() {
+  const [milestones, setMilestones] = useState([]);
+
+  const { user } = useAuth();
+
+  const getAllTheMilestones = () => {
+    getMilestones(user.uid).then(setMilestones);
+  };
+
+  useEffect(() => {
+    getAllTheMilestones();
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/team/new" passHref>
+        <Button>Add A Milestone</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {milestones.map((milestone) => (
+          <MilestoneCard key={milestone.firebaseKey} milestoneObj={milestone} onUpdate={getAllTheMilestones} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ShowMilestones;
